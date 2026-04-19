58d0c8ff728fc684e63f044335eee6bdd601df45
grotesk <groteskzp@gmail.com>
2026-04-19 05:59:35 +0300

    Update SimpleLightbox import to use named export and clean up unused import

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -64,8 +64,9 @@ const images = [
  },
 ];
 
+import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
 
-import SimpleLightbox from "simplelightbox";
+// import SimpleLightbox from "simplelightbox";
 
 import "simplelightbox/dist/simple-lightbox.min.css";
 
